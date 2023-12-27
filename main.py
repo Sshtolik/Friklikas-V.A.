f2359b33b946e682712e44ed9a82bf5aa8ab4051
Sshtolik <151021605+Sshtolik@users.noreply.github.com>
2023-12-27 14:52:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import telebot
+from telebot import types
+from geopy.geocoders import Nominatim
 
 TOKEN = '6711340991:AAGLFGIo_-4dprRPA0wz_bnJ1agcLyXXGnI'
 bot = telebot.TeleBot(TOKEN)
@@ -16,5 +18,26 @@ def sticker_from_user(message):
                                       f'\nТеперь я могу использовать его &#128522;',
                      parse_mode='HTML')
     bot.send_sticker(message.chat.id, sticker_id)
-
+@bot.message_handler(commands=['place'])
+def place(message):
+    bot.send_message(message.chat.id, 'Нашёл для тебя интересное место:')
+    bot.send_location(message.chat.id, 55.760382, 37.618255)
+@bot.message_handler(commands=['place1'])
+def place1(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    button_geo = types.KeyboardButton(text='Разрешить доступ к геолокации',
+                                      request_location=True)
+    keyboard.add(button_geo)
+    bot.send_message(message.chat.id,
+                     'Я умею работать с геолокацией! \nНо для начала мне нужно твое разрешение',
+                     reply_markup=keyboard)
+@bot.message_handler(content_types=['location'])
+def location_from_user(message):
+    bot.send_message(message.chat.id, 'Отличное место! Обязательно добавлю его в свой список')
+@bot.message_handler(commands=['place2'])
+def place(message):
+    geolocation = Nomination(user_agent="GetLoc")
+    location_name = geolocation.reverse('55.760382, 37.618255')
+    bot.send_message(message.chat.id,
+                     f'Выбранное место расположено по адресу: \n{location_name.address}')
 bot.polling(none_stop=True)
