8a7a823d2ad6e969fe4993c1c452d08dc2398a27
Sshtolik <151021605+Sshtolik@users.noreply.github.com>
2023-12-27 14:27:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ def send_sticker(message):
     hi_sticker = open('hi.png', 'rb')
     bot.send_sticker(message.chat.id, hi_sticker)
     bot.send_message(message.chat.id, 'Привет! &#128516;', parse_mode='HTML')
+@bot.message_handler(content_types=['sticker'])
+def sticker_from_user(message):
+    sticker_id = message.sticker.file_id
+    bot.send_message(message.chat.id, f'Отличный стикер! &#128513; '
+                                      f'\nВот его id: \n<b>{sticker_id}</b>'
+                                      f'\nТеперь я могу использовать его &#128522;',
+                     parse_mode='HTML')
+    bot.send_sticker(message.chat.id, sticker_id)
 
 bot.polling(none_stop=True)
